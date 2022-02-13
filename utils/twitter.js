ede6c72fbfe7f2e16ed5bafec1482abc775a7146
MugishaU <muwiragiye123@gmail.com>
2022-02-13 15:21:11 +0000

    refactor to use async-await

--- utils/twitter.js
+++ utils/twitter.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const axios = require("axios").default
 
-const fetch = (searchTerm, nextToken = null, tweets = [], index = 1) => {
+const fetch = async (searchTerm, nextToken = null, tweets = [], index = 1) => {
 	const bearerToken = process.env.TWITTER_BEARER_TOKEN
 	const searchUrl = `https://api.twitter.com/2/tweets/search/recent?query=${searchTerm}`
 	const nextTokenQuery = nextToken ? `&next_token=${nextToken}` : ""
@@ -12,27 +12,27 @@ const fetch = (searchTerm, nextToken = null, tweets = [], index = 1) => {
 		},
 	}
 
-	axios
-		.get(urlWithNextToken, options)
-		.then((response) => {
-			const apiResults = tweets
+	try {
+		const response = await axios.get(urlWithNextToken, options)
+		const currentResults = tweets
+		// count is to protect the API, a maximum of 50 tweets are collected.
+		const count = index + 1
 
-			if (response.data.data) {
-				apiResults.push(response.data.data)
-			}
+		if (response.data.data) {
+			currentResults.push(response.data.data)
+		}
 
-			//count is to protect the API, a maximum of 50 tweets are collected.
-			const count = index + 1
-
-			if (response.data.meta.next_token && index < 5) {
-				fetch(searchTerm, response.data.meta.next_token, apiResults, count)
-			} else {
-				console.log(tweets.flat())
-			}
+		if (response.data.meta.next_token && index < 5) {
+			fetch(searchTerm, response.data.meta.next_token, currentResults, count)
+		} else {
+			console.log(tweets.flat())
+		}
+	} catch (error) {
+		console.log({
+			errorCode: error.code ? error.code : error.response.status,
+			errorMessage: error.message,
 		})
-		.catch((error) => {
-			console.log(error)
-		})
+	}
 }
 
 exports.fetch = fetch
