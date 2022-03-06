10c0e84c1f5cc4562f4f14116189c20ae9839289
MugishaU <muwiragiye123@gmail.com>
2022-03-06 22:21:20 +0000

    format lambda terraform

--- terraform/modules/authorisation-lambda/lambda.tf
+++ terraform/modules/authorisation-lambda/lambda.tf
@@ -1,5 +1,3 @@
-# data "aws_caller_identity" "current" {}
-
 resource "aws_lambda_function" "twitter-authorisation-lambda" {
   filename         = "../authorisation-lambda/authorisation-lambda.zip"
   function_name    = var.auth-lambda-name
@@ -15,6 +13,5 @@ resource "aws_lambda_permission" "api-gateway-lambda-permission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.twitter-authorisation-lambda.function_name
   principal     = "apigateway.amazonaws.com"
-
-  source_arn = "arn:aws:execute-api:eu-west-2:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.twitter-auth-api-gateway-terraform.id}/*/*/*"
+  source_arn    = "arn:aws:execute-api:eu-west-2:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.twitter-auth-api-gateway-terraform.id}/*/*/*"
 }
