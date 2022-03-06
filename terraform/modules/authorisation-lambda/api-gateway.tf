928c03fff1a9ca18930d0e94108771aae7265b97
MugishaU <muwiragiye123@gmail.com>
2022-03-06 22:19:22 +0000

    Add API Gateway Routes

--- terraform/modules/authorisation-lambda/api-gateway.tf
+++ terraform/modules/authorisation-lambda/api-gateway.tf
@@ -18,3 +18,27 @@ resource "aws_apigatewayv2_integration" "twitter-auth-api-gateway-lambda-integra
   integration_uri        = aws_lambda_function.twitter-authorisation-lambda.invoke_arn
   payload_format_version = "2.0"
 }
+
+resource "aws_apigatewayv2_route" "twitter-auth-api-gateway-authorise-route" {
+  api_id    = aws_apigatewayv2_api.twitter-auth-api-gateway-terraform.id
+  route_key = "GET /authorise"
+  target    = "integrations/${aws_apigatewayv2_integration.twitter-auth-api-gateway-lambda-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "twitter-auth-api-gateway-callback-route" {
+  api_id    = aws_apigatewayv2_api.twitter-auth-api-gateway-terraform.id
+  route_key = "POST /callback"
+  target    = "integrations/${aws_apigatewayv2_integration.twitter-auth-api-gateway-lambda-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "twitter-auth-api-gateway-token-route" {
+  api_id    = aws_apigatewayv2_api.twitter-auth-api-gateway-terraform.id
+  route_key = "POST /token"
+  target    = "integrations/${aws_apigatewayv2_integration.twitter-auth-api-gateway-lambda-integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "twitter-auth-api-gateway-refresh-route" {
+  api_id    = aws_apigatewayv2_api.twitter-auth-api-gateway-terraform.id
+  route_key = "POST /refresh"
+  target    = "integrations/${aws_apigatewayv2_integration.twitter-auth-api-gateway-lambda-integration.id}"
+}
