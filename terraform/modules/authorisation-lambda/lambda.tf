fbac694f28f08c603fb9fe288cf39e96b53e3942
MugishaU <muwiragiye123@gmail.com>
2022-03-10 20:08:30 +0000

    move terraform state to terraform cloud

--- terraform/modules/authorisation-lambda/lambda.tf
+++ terraform/modules/authorisation-lambda/lambda.tf
@@ -1,11 +1,10 @@
 resource "aws_lambda_function" "twitter-authorisation-lambda" {
-  filename         = "../authorisation-lambda/authorisation-lambda.zip"
-  function_name    = var.auth-lambda-name
-  role             = aws_iam_role.iam-role-authorisation-lambda.arn
-  handler          = "index.handler"
-  source_code_hash = filebase64sha256("../authorisation-lambda/authorisation-lambda.zip")
-  runtime          = "nodejs14.x"
-  timeout          = 60
+  filename      = "../authorisation-lambda/authorisation-lambda.zip"
+  function_name = var.auth-lambda-name
+  role          = aws_iam_role.iam-role-authorisation-lambda.arn
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  timeout       = 60
 }
 
 resource "aws_lambda_permission" "api-gateway-lambda-permission" {
