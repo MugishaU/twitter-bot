6a24191d8d6467f67b59374201c82aa388a6f204
MugishaU <muwiragiye123@gmail.com>
2022-03-27 22:37:04 +0100

    update script to handle typescript transpilation

--- scripts/update-lambda.sh
+++ scripts/update-lambda.sh
@@ -3,6 +3,20 @@
 FUNCTION_NAME=$1
 ZIP_NAME=$2
 
+tsc
+
+TYPESCRIPT_STATUS=$?
+
+if [ $TYPESCRIPT_STATUS -eq 0 ]
+then
+  echo "Typescript transpilation successful!"
+else
+  echo "Typescript transpilation failed." >&2
+  exit 1
+fi
+
+cd .out
+
 zip -r ${ZIP_NAME}.zip . -x ".*" -x "package*" -x "credentials/*"
 
 aws lambda update-function-code --function-name ${FUNCTION_NAME} --zip-file fileb://${ZIP_NAME}.zip
