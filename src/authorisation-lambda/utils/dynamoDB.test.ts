59bc8a3d6d713654048eeb2cb8a98cf632748523
MugishaU <muwiragiye123@gmail.com>
2022-04-03 02:26:47 +0100

    add test to getItem

--- src/authorisation-lambda/utils/dynamoDB.test.ts
+++ src/authorisation-lambda/utils/dynamoDB.test.ts
@@ -2,11 +2,10 @@ import { mockClient } from "aws-sdk-client-mock"
 import {
 	DynamoDBClient,
 	GetItemCommand,
-	GetItemCommandOutput,
-	PutItemCommand,
+	PutItemCommand
 } from "@aws-sdk/client-dynamodb"
 
-import { getItem } from "./dynamoDB"
+import { getItem, putItem } from "./dynamoDB"
 
 const ddbMock = mockClient(DynamoDBClient)
 
@@ -15,50 +14,69 @@ describe("getItem", () => {
 		ddbMock.reset()
 	})
 
-	describe("when given an appropriate input", () => {
-		it("should successfully return the item if in DynamoDb", async () => {
-			ddbMock
-				.on(GetItemCommand, {
-					TableName: "test-table",
-					Key: {
-						id: {
-							S: "0",
-						},
-					},
-				})
-				.resolves({
-					Item: {
-						id: {
-							S: "0",
-						},
-						foo: {
-							S: "bar",
-						},
-					},
-					$metadata: { httpStatusCode: 200 },
-				})
-			const item = await getItem("test-table", { id: "0" })
-			expect(item).toStrictEqual({
-				statusCode: 200,
-				body: { id: "0", foo: "bar" },
+	it("should successfully return the item if in DynamoDb", async () => {
+		ddbMock
+			.on(GetItemCommand, {
+				TableName: "test-table",
+				Key: {
+					id: {
+						S: "0"
+					}
+				}
 			})
-		})
-
-		it("should successfully return undefined if not in DynamoDb", async () => {
-			ddbMock
-				.on(GetItemCommand, {
-					TableName: "test-table",
-					Key: {
-						id: {
-							S: "0",
-						},
+			.resolves({
+				Item: {
+					id: {
+						S: "0"
 					},
-				})
-				.resolves({
-					$metadata: { httpStatusCode: 200 },
-				})
-			const item = await getItem("test-table", { id: "0" })
-			expect(item).toStrictEqual({ statusCode: 200, body: undefined })
+					foo: {
+						S: "bar"
+					}
+				},
+				$metadata: { httpStatusCode: 200 }
+			})
+		const item = await getItem("test-table", { id: "0" })
+		expect(item).toStrictEqual({
+			statusCode: 200,
+			body: { id: "0", foo: "bar" }
+		})
+	})
+
+	it("should successfully return undefined if not in DynamoDb", async () => {
+		ddbMock
+			.on(GetItemCommand, {
+				TableName: "test-table",
+				Key: {
+					id: {
+						S: "0"
+					}
+				}
+			})
+			.resolves({
+				$metadata: { httpStatusCode: 200 }
+			})
+		const item = await getItem("test-table", { id: "0" })
+		expect(item).toStrictEqual({ statusCode: 200, body: undefined })
+	})
+
+	it("should return an error correctly", async () => {
+		ddbMock
+			.on(GetItemCommand, {
+				TableName: "test-table",
+				Key: {
+					id: {
+						S: "0"
+					}
+				}
+			})
+			.rejects({
+				$metadata: { httpStatusCode: 400 },
+				message: "Requested resource not found"
+			})
+		const item = await getItem("test-table", { id: "0" })
+		expect(item).toStrictEqual({
+			statusCode: 400,
+			errorMessage: "Requested resource not found"
 		})
 	})
 })
