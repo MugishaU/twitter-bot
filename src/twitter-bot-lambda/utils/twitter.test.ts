454380e01139a484c55d8248abb7b803d343f1ec
MugishaU <muwiragiye123@gmail.com>
2022-04-09 23:18:34 +0100

    update test

--- src/twitter-bot-lambda/utils/twitter.test.ts
+++ src/twitter-bot-lambda/utils/twitter.test.ts
@@ -18,13 +18,14 @@ describe("fetchTweets", () => {
 
 	it("should successfully return tweets", async () => {
 		axiosMock.get.mockResolvedValue({
-			data: { data: [{ id: "0", text: "foo" }], meta: {} }
+			status: 200,
+			data: { data: [{ id: "0", text: "foo" }] }
 		})
 
 		const result = await fetchTweets("bar")
 
 		expect(axiosMock.get).toHaveBeenCalledWith(
-			"https://api.twitter.com/2/tweets/search/recent?query=bar",
+			"https://api.twitter.com/2/tweets/search/recent?max_results=50&query=bar",
 			{ headers: { Authorization: "BEARER test-bearer-token" } }
 		)
 		expect(result).toStrictEqual({
@@ -32,28 +33,5 @@ describe("fetchTweets", () => {
 			message: "Tweets fetched successfully.",
 			tweets: [{ id: "0", text: "foo" }]
 		})
-	})
-
-	it("should successfully return paginated results", async () => {
-		axiosMock.get
-			.mockResolvedValueOnce({
-				data: { data: [{ id: "0", text: "foo" }], meta: { next_token: "0" } }
-			})
-			.mockResolvedValueOnce({
-				data: { data: [{ id: "1", text: "bar" }], meta: {} }
-			})
-
-		const result = await fetchTweets("bar")
-
-		expect(axiosMock.get).toHaveBeenNthCalledWith(
-			1,
-			"https://api.twitter.com/2/tweets/search/recent?query=bar",
-			{ headers: { Authorization: "BEARER test-bearer-token" } }
-		)
-		expect(axiosMock.get).toHaveBeenNthCalledWith(
-			2,
-			"https://api.twitter.com/2/tweets/search/recent?query=bar&next_token=0",
-			{ headers: { Authorization: "BEARER test-bearer-token" } }
-		)
 	})
 })
