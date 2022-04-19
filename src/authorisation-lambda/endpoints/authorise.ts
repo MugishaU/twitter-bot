9cc7f07f3feb2a70d8dfc7d06ab48522866bf042
MugishaU <muwiragiye123@gmail.com>
2022-04-19 21:08:22 +0100

    add authorise endpoint functionality

--- src/authorisation-lambda/endpoints/authorise.ts
+++ src/authorisation-lambda/endpoints/authorise.ts
@@ -1,2 +1,30 @@
 import { generateUrl } from "../utils/ouathUrl"
 import { putItem } from "../utils/dynamoDB"
+
+export const authorise = async (): Promise<string | null> => {
+	const url = generateUrl(
+		"https://pw7fshn6z7.execute-api.eu-west-2.amazonaws.com/callback",
+		["tweet.read", "tweet.write", "users.read", "offline.access"]
+	)
+
+	console.log(url.codeVerifier)
+
+	const putState = putItem("twitter-auth", {
+		id: "state",
+		value: url.state
+	})
+
+	const putCodeVerifier = putItem("twitter-auth", {
+		id: "codeVerifier",
+		value: url.codeVerifier
+	})
+
+	const stateResult = await putState
+	const codeVerifierResult = await putCodeVerifier
+
+	if (stateResult.statusCode == 200 && codeVerifierResult.statusCode == 200) {
+		return url.oauthUrl
+	}
+
+	return null
+}
