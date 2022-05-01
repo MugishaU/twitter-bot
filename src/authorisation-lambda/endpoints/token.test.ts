5d8ad1377bf3cc4533c1c19218bf2ddfcec33ee4
MugishaU <muwiragiye123@gmail.com>
2022-05-02 00:35:32 +0100

    add token test for fail to save to db

--- src/authorisation-lambda/endpoints/token.test.ts
+++ src/authorisation-lambda/endpoints/token.test.ts
@@ -9,6 +9,7 @@ import { token } from "./token"
 //State not found on DB
 //Code not found on DB
 //State and/or Code Not Provided query params
+//Axios error
 
 jest.mock("axios")
 const axiosMock = axios as jest.Mocked<typeof axios>
@@ -30,18 +31,48 @@ describe("token", () => {
 				body: { id: "codeVerifier", value: "codeVerifier" }
 			})
 
-		jest.spyOn(dynamoDB, "putItem").mockResolvedValue({ statusCode: 200 })
-
 		axiosMock.post.mockResolvedValueOnce({
 			status: 200,
 			data: { access_token: "accessToken", refresh_token: "refreshToken" }
 		})
+
+		jest.spyOn(dynamoDB, "putItem").mockResolvedValue({ statusCode: 200 })
 
 		const result = await token({ state: "state", code: "code" })
 
 		expect(result).toStrictEqual({
 			statusCode: 200,
 			body: "Access Token and Refresh Token saved to authorisation server."
+		})
+	})
+
+	it("should fail if the Access or Refresh token fail to save to DynamoDb", async () => {
+		jest
+			.spyOn(dynamoDB, "getItem")
+			.mockResolvedValueOnce({
+				statusCode: 200,
+				body: { id: "state", value: "state" }
+			})
+			.mockResolvedValueOnce({
+				statusCode: 200,
+				body: { id: "codeVerifier", value: "codeVerifier" }
+			})
+
+		axiosMock.post.mockResolvedValueOnce({
+			status: 200,
+			data: { access_token: "accessToken", refresh_token: "refreshToken" }
+		})
+
+		jest
+			.spyOn(dynamoDB, "putItem")
+			.mockResolvedValueOnce({ statusCode: 200 })
+			.mockResolvedValueOnce({ statusCode: 500 })
+
+		const result = await token({ state: "state", code: "code" })
+
+		expect(result).toStrictEqual({
+			statusCode: 500,
+			body: "Error saving Access Token and Refresh Token to authorisation server."
 		})
 	})
 })
