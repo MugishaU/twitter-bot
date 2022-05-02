d3fdc1a862c1c24d54a3868705261cae0abcf2cd
MugishaU <muwiragiye123@gmail.com>
2022-05-02 16:55:38 +0100

    finish testing token.ts

--- src/authorisation-lambda/endpoints/token.test.ts
+++ src/authorisation-lambda/endpoints/token.test.ts
@@ -2,16 +2,13 @@ import axios from "axios"
 import * as dynamoDB from "../utils/dynamoDB"
 import { token } from "./token"
 
-//Code not found on DB
-//State and/or Code Not Provided query params
-//Axios error
-
 jest.mock("axios")
 const axiosMock = axios as jest.Mocked<typeof axios>
 
 describe("token", () => {
 	beforeEach(() => {
 		jest.clearAllMocks()
+		jest.restoreAllMocks()
 	})
 
 	it("should return a success if the Access and Refresh tokens are saved to DynamoDb", async () => {
@@ -84,7 +81,7 @@ describe("token", () => {
 			})
 
 		axiosMock.post.mockResolvedValueOnce({
-			status: 404
+			status: 200
 		})
 
 		const result = await token({ state: "state", code: "code" })
@@ -131,21 +128,76 @@ describe("token", () => {
 	})
 
 	it("should fail if state is not found in DynamoDb", async () => {
-		jest
-			.spyOn(dynamoDB, "getItem")
-			.mockResolvedValueOnce({
-				statusCode: 404
-			})
-			.mockResolvedValueOnce({
-				statusCode: 200,
-				body: { id: "codeVerifier", value: "codeVerifier" }
-			})
+		jest.spyOn(dynamoDB, "getItem").mockResolvedValueOnce({
+			statusCode: 404
+		})
 
 		const result = await token({ state: "state", code: "code" })
 
 		expect(result).toStrictEqual({
 			statusCode: 404,
 			body: "State not found on authorisation server."
+		})
+	})
+
+	it("should fail if code verifier is not found in DynamoDb", async () => {
+		jest
+			.spyOn(dynamoDB, "getItem")
+			.mockResolvedValueOnce({
+				statusCode: 200,
+				body: { id: "state", value: "state" }
+			})
+			.mockResolvedValueOnce({
+				statusCode: 404
+			})
+
+		const result = await token({ state: "state", code: "code" })
+
+		expect(result).toStrictEqual({
+			statusCode: 404,
+			body: "Code Verifier not found on authorisation server."
+		})
+	})
+
+	it("should fail if the state or code isn't present in the callback query parameters", async () => {
+		const resultNoState = await token({ code: "code" })
+		const resultNoCode = await token({ state: "state" })
+
+		expect(resultNoCode).toStrictEqual({
+			statusCode: 404,
+			body: "State and/or code not provided."
+		})
+
+		expect(resultNoState).toStrictEqual({
+			statusCode: 404,
+			body: "State and/or code not provided."
+		})
+	})
+
+	it("should fail if the call to the Twitter API fails", async () => {
+		jest
+			.spyOn(dynamoDB, "getItem")
+			.mockResolvedValueOnce({
+				statusCode: 200,
+				body: { id: "state", value: "state" }
+			})
+			.mockResolvedValueOnce({
+				statusCode: 200,
+				body: { id: "codeVerifier", value: "codeVerifier" }
+			})
+
+		axiosMock.post.mockRejectedValue({
+			response: { status: 500 },
+			message: "Generic Axios Error Message"
+		})
+
+		jest.spyOn(dynamoDB, "putItem").mockResolvedValue({ statusCode: 200 })
+
+		const result = await token({ state: "state", code: "code" })
+
+		expect(result).toStrictEqual({
+			statusCode: 500,
+			body: "Generic Axios Error Message"
 		})
 	})
 })
