294ed599d6a4b432e58a14bd02afe57032b54246
MugishaU <muwiragiye123@gmail.com>
2022-05-06 12:10:55 +0100

    improve lambda deployment script outputs

--- scripts/update-lambda.sh
+++ scripts/update-lambda.sh
@@ -3,23 +3,34 @@
 FUNCTION_NAME=$1
 ZIP_NAME=$2
 
+
+report_status () {
+  STATUS=$1
+  SUCCESS_MESSAGE=$2
+  FAILURE_MESSAGE=$3
+
+  if [ $STATUS -eq 0 ]
+  then
+    echo $SUCCESS_MESSAGE
+  else
+    echo $FAILURE_MESSAGE >&2
+    exit 1
+  fi
+}
+
 tsc
 
-TYPESCRIPT_STATUS=$?
-
-if [ $TYPESCRIPT_STATUS -eq 0 ]
-then
-  echo "Typescript transpilation successful!"
-else
-  echo "Typescript transpilation failed." >&2
-  exit 1
-fi
+report_status $? "Typescript transpilation successful!" "Typescript transpilation failed."
 
 cp -r node_modules .out
 
+report_status $? "Node modules import successful!" "Node modules import failed."
+
 cd .out
 
-zip -r ${ZIP_NAME}.zip .
+zip -r -q ${ZIP_NAME}.zip .
+
+report_status $? "Deployment package zip file creation successful!" "Deployment package zip file creation failed."
 
 aws lambda update-function-code --function-name ${FUNCTION_NAME} --zip-file fileb://${ZIP_NAME}.zip | jq '.Environment = "REDACTED"'
 
