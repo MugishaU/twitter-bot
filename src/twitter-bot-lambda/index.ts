e0bda4bacafbdc38292ee91c5ff5b2fb87db6912
MugishaU <muwiragiye123@gmail.com>
2022-05-15 23:45:34 +0100

    random eventbridge implementation

--- src/twitter-bot-lambda/index.ts
+++ src/twitter-bot-lambda/index.ts
@@ -1,11 +1,18 @@
+import { EventBridgeEvent, APIGatewayProxyResult } from "aws-lambda"
 import { fetchTweets } from "./utils/twitter"
 
-const queries = [
-	"%23BlackInTech apply now -is:retweet",
-	"%23BlackTechTwitter apply now -is:retweet",
-	"%23BlackWomenInTech apply now -is:retweet",
-]
+// const queries = [
+// 	"%23BlackInTech apply now -is:retweet",
+// 	"%23BlackTechTwitter apply now -is:retweet",
+// 	"%23BlackWomenInTech apply now -is:retweet"
+// ]
 
-for (let query of queries) {
-	fetchTweets(query)
+// for (let query of queries) {
+// 	fetchTweets(query).then((x) => console.log(x))
+// }
+
+export const handler = async (
+	event: EventBridgeEvent<string, string>
+): Promise<any> => {
+	return event.detail
 }
