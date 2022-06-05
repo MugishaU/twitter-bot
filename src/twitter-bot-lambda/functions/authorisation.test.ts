89225ae6f3609be1de1283fadf21c91b81c814dd
MugishaU <muwiragiye123@gmail.com>
2022-06-05 20:10:25 +0000

    add getToken tests

--- src/twitter-bot-lambda/functions/authorisation.test.ts
+++ src/twitter-bot-lambda/functions/authorisation.test.ts
@@ -1,10 +1,71 @@
-import { getToken, refreshToken } from "./authorisation";
+import * as auth from "./authorisation"
+import * as dynamoDB from "../utils/dynamoDB"
 
-//getToken
-//Access is successful & ttl is valid
-//Access is successful & ttl is invalid && refesh works
-//Access is successful & ttl is invalid && refresh fails
-//Access is not successful
+describe("getToken", () => {
+	beforeEach(() => {
+		jest.clearAllMocks()
+		jest.resetAllMocks()
+	})
+
+	it("should return an access token if fetched successfully", async () => {
+		jest.spyOn(dynamoDB, "getItem").mockResolvedValueOnce({
+			statusCode: 200,
+			body: { id: "accessToken", value: "accessTokenValue" }
+		})
+
+		const result = await auth.getToken()
+		expect(result).toBe("accessTokenValue")
+	})
+
+	it("should return an access token if the refresh is successful", async () => {
+		jest
+			.spyOn(dynamoDB, "getItem")
+			.mockResolvedValueOnce({
+				statusCode: 500
+			})
+			.mockResolvedValueOnce({
+				statusCode: 200,
+				body: { id: "accessToken", value: "accessTokenValue" }
+			})
+
+		jest.spyOn(auth, "refreshToken").mockResolvedValue(200)
+
+		const result = await auth.getToken()
+		expect(result).toBe("accessTokenValue")
+	})
+
+	it("should return an empty string if the refresh is unsuccessful", async () => {
+		jest
+			.spyOn(dynamoDB, "getItem")
+			.mockResolvedValueOnce({
+				statusCode: 500
+			})
+			.mockResolvedValueOnce({
+				statusCode: 500
+			})
+
+		jest.spyOn(auth, "refreshToken").mockResolvedValue(500)
+
+		const result = await auth.getToken()
+		expect(result).toBe("")
+	})
+
+	it("should return an empty string if the refresh is successful but second access token fetch fails", async () => {
+		jest
+			.spyOn(dynamoDB, "getItem")
+			.mockResolvedValueOnce({
+				statusCode: 500
+			})
+			.mockResolvedValueOnce({
+				statusCode: 500
+			})
+
+		jest.spyOn(auth, "refreshToken").mockResolvedValue(200)
+
+		const result = await auth.getToken()
+		expect(result).toBe("")
+	})
+})
 
 //refreshToken
 //Refresh is successful
