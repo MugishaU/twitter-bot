15337ba96f95c6e20ff2c4c8a0291d6a7009a120
MugishaU <muwiragiye123@gmail.com>
2022-06-08 00:56:41 +0100

    begin to implement lambda

--- src/twitter-bot-lambda/index.ts
+++ src/twitter-bot-lambda/index.ts
@@ -1,5 +1,6 @@
-import { fetchTweets } from "./utils/twitter"
-
+import { fetchTweets, retweet } from "./utils/twitter"
+import { getToken } from "./functions/authorisation"
+import { putItem } from "./utils/dynamoDB"
 
 // const queries = [
 // 	"%23BlackInTech apply now -is:retweet",
@@ -17,4 +18,17 @@ interface CloudWatchEvent {
 
 export const handler = async (event: CloudWatchEvent): Promise<void> => {
 	console.log(`Lambda Invoked. Payload: ${event.query}`)
+	const bearerToken = await getToken()
+
+	const searchResult = await fetchTweets(event.query, bearerToken)
+
+	if (searchResult.tweets) {
+		for (const tweet of searchResult.tweets) {
+			const saveTweet = await putItem("twitter-history", {
+				id: tweet.id,
+				value: tweet.text
+			})
+			console.log(saveTweet.statusCode)
+		}
+	}
 }
