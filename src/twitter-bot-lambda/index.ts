7531e06ada4ce8711b8b70b7545ad4d7e86a9dcf
MugishaU <muwiragiye123@gmail.com>
2022-06-29 13:12:57 +0100

    remove comments

--- src/twitter-bot-lambda/index.ts
+++ src/twitter-bot-lambda/index.ts
@@ -2,16 +2,6 @@ import { fetchTweets, retweet } from "./utils/twitter"
 import { getToken } from "./functions/authorisation"
 import { getItem, putItem, checkDynamoDbResult } from "./utils/dynamoDB"
 
-// const queries = [
-// 	"%23BlackInTech apply now -is:retweet",
-// 	"%23BlackTechTwitter apply now -is:retweet",
-// 	"%23BlackWomenInTech apply now -is:retweet"
-// ]
-
-// for (let query of queries) {
-// 	fetchTweets(query).then((x) => console.log(x))
-// }
-
 interface CloudWatchEvent {
 	query: string
 }
