9c366ab5a764b0b197f97f6e134a5c93c4d0fda6
MugishaU <muwiragiye123@gmail.com>
2022-07-01 20:08:00 +0100

    add more queries

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,26 +11,50 @@ module "dynamoDB" {
   source = "./modules/dynamoDB"
 }
 
-module "eventBridgeRule0" {
+module "eventBridgeRule1" {
   source              = "./modules/eventBridge"
   query               = "%23BlackInTech apply now -is:retweet"
+  function_name       = module.twitter-bot-lambda.name
+  lambda-arn          = module.twitter-bot-lambda.arn
+  schedule_expression = "cron(00 06 * * ? *)"
+}
+
+module "eventBridgeRule2" {
+  source              = "./modules/eventBridge"
+  query               = "%23BlackTechTwitter apply now -is:retweet"
+  function_name       = module.twitter-bot-lambda.name
+  lambda-arn          = module.twitter-bot-lambda.arn
+  schedule_expression = "cron(00 09 * * ? *)"
+}
+
+module "eventBridgeRule3" {
+  source              = "./modules/eventBridge"
+  query               = "%23BlackWomenInTech apply now -is:retweet"
   function_name       = module.twitter-bot-lambda.name
   lambda-arn          = module.twitter-bot-lambda.arn
   schedule_expression = "cron(00 12 * * ? *)"
 }
 
-module "eventBridgeRule1" {
+module "eventBridgeRule4" {
   source              = "./modules/eventBridge"
-  query               = "%23BlackTechTwitter apply now -is:retweet"
+  query               = "apply from:codingblackfems -is:retweet"
   function_name       = module.twitter-bot-lambda.name
   lambda-arn          = module.twitter-bot-lambda.arn
   schedule_expression = "cron(00 15 * * ? *)"
 }
 
-module "eventBridgeRule2" {
+module "eventBridgeRule5" {
   source              = "./modules/eventBridge"
-  query               = "%23BlackWomenInTech apply now -is:retweet"
+  query               = "apply from:codefirstgirls -is:retweet"
   function_name       = module.twitter-bot-lambda.name
   lambda-arn          = module.twitter-bot-lambda.arn
   schedule_expression = "cron(00 18 * * ? *)"
+}
+
+module "eventBridgeRule6" {
+  source              = "./modules/eventBridge"
+  query               = "apply from:btpipeline -is:retweet"
+  function_name       = module.twitter-bot-lambda.name
+  lambda-arn          = module.twitter-bot-lambda.arn
+  schedule_expression = "cron(00 21 * * ? *)"
 }
